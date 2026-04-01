d949804cfd39c03c476886c0cda87d054a7da747
koguchi-e <koguchi.e915@gmail.com>
2026-04-02 08:27:51 +0900

    matching_login_user?をコールバックに戻す

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@
 
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
-
+  before_action :matching_login_user?, only: %i[edit update destroy]
   before_action :set_blog, only: %i[show edit update destroy]
 
   def index
@@ -17,9 +17,7 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
-  def edit
-    matching_login_user?
-  end
+  def edit; end
 
   def create
     @blog = current_user.blogs.new(blog_params)
@@ -32,8 +30,6 @@ class BlogsController < ApplicationController
   end
 
   def update
-    matching_login_user?
-
     if @blog.update(blog_params)
       redirect_to blog_url(@blog), notice: 'Blog was successfully updated.'
     else
@@ -42,7 +38,6 @@ class BlogsController < ApplicationController
   end
 
   def destroy
-    matching_login_user?
     @blog.destroy!
 
     redirect_to blogs_url, notice: 'Blog was successfully destroyed.', status: :see_other
